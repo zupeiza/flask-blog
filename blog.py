478577111753dfee46917dbeda535f63cb5e759e
zupeiza <zupeiza@gmail.com>
2016-03-21 13:41:56 +0100

    Add posts

--- blog.py
+++ blog.py
@@ -58,5 +58,22 @@ def main():
 	g.db.close()
 	return render_template('main.html', posts=posts)
 
+@app.route('/add', methods=['POST'])
+@login_required
+def add():
+	title = request.form['title']
+	post = request.form['post']
+	if not title or not post:
+		flash('All fields are required. Please try again')
+		return redirect(url_for('main'))
+	else:
+		g.db = connect_db()
+		g.db.execute('insert into posts (title, post) values (?, ?)',
+			[request.form['title'], request.form['post']])
+		g.db.commit()
+		g.db.close()
+		flash('New entry was successfully posted!')
+		return redirect(url_for('main'))
+
 if __name__ == '__main__':
 	app.run(debug = True)
