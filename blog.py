4a4395525a4c28b31d3d2106f1212f85d30321cf
zupeiza <zupeiza@gmail.com>
2016-03-21 13:26:22 +0100

    Sessions and login required

--- blog.py
+++ blog.py
@@ -3,8 +3,8 @@
 #imports
 from flask import Flask, render_template, request, session, \
 	flash, redirect, url_for, g
-
 import sqlite3
+from functools import wraps
 
 #configuration
 DATABASE = 'blog.db'
@@ -20,6 +20,16 @@ app.config.from_object(__name__)
 #function used for connecting to the database
 def connect_db():
 	return sqlite3.connect(app.config['DATABASE'])
+
+def login_required(test):
+	@wraps(test)
+	def wrap(*args, **kwargs):
+		if 'logged_in' in session:
+			return test(*args, **kwargs)
+		else:
+			flash('You need to login first.')
+			return redirect(url_for('login'))
+	return wrap
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
@@ -40,6 +50,7 @@ def logout():
 	return redirect(url_for('login'))
 
 @app.route('/main')
+@login_required
 def main():
 	return render_template('main.html')
 
